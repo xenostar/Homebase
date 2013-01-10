6971da9ceb398217590b688fa6e267b3c7deeed0
Xenostar <bryan.a.leewood@gmail.com>
2013-01-10 14:31:36 -0500

    Adding a bunch of new files

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -7,13 +7,4 @@
 ----------------------------------------------- */
 $(function(){
 
-  // Setting scroll to top
-  $("a[href='#top']").click(function() {
-    $("html, body").animate({ scrollTop: 0 }, "slow");
-    return false;
-  });
-
-  // Assigning FitText to header
-  $("#header h1").fitText(0.49);
-
 });
