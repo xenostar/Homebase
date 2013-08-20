f75e3c3ddee31b1a956402f0a346a91e65ac3ba0
Bryan <bryan.a.leewood@gmail.com>
2013-08-20 13:53:37 -0400

    Quite a few updates

---
+++ assets/scripts/scripts_top.js
@@ -0,0 +1,63 @@
+/* -------------------------------------------------------------------------------------------------
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// SCRIPTS_TOP.JS : SCRIPTS TO BE PLACED AT THE TOP OF THE PAGE
+////////////////////////////////////////////////////////////////////////////////////////////////////
+------------------------------------------------------------------------------------------------- */
+/* Images to preload
+-----------------------------------------------
+/////////////////////////////////////////////// */
+var heroArray = [
+	//'/uploads/hero_about.jpg',
+]
+
+
+
+/* Functions
+-----------------------------------------------
+/////////////////////////////////////////////// */
+function imgLoaded(img){
+	$(img).parent().addClass('loaded');
+};
+function preCache(){
+	$.each(heroArray, function(){
+		var img = new Image();
+		img.src = this;
+	});
+};
+function draw() {
+	var canvas = document.getElementById("canvas");
+	if (canvas.getContext) {
+		var ctx = canvas.getContext("2d");
+
+		ctx.fillStyle = "rgb(200,0,0)";
+		ctx.fillRect (10, 10, 55, 50);
+
+		ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+		ctx.fillRect (30, 30, 55, 50);
+	}
+}
+
+
+/* DOCUMENT.READY - Fires as soon as the DOM is ready
+-----------------------------------------------
+/////////////////////////////////////////////// */
+$(document).ready(function() {
+
+	// Code Here
+
+});
+
+
+
+/* DOCUMENT.LOAD - Fires only when all content for the page has been downloaded and rendered
+-----------------------------------------------
+/////////////////////////////////////////////// */
+$(window).load(function() {
+
+	// Canvas
+	draw();
+
+	// Leave this last in here
+	preCache();
+
+});
